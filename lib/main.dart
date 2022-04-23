84abb372ea65e47cde90f7b580ab27840e1ad544
prasetion <prasetion@gmail.com>
2022-04-23 15:54:17 +0700

    text controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:personal_expenses/transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -29,6 +30,9 @@ class MyHomePage extends StatelessWidget {
     ),
   ];
 
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,7 +40,7 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        // mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
@@ -45,6 +49,36 @@ class MyHomePage extends StatelessWidget {
               color: Colors.blue,
               child: Text("CHART"),
               elevation: 5,
+            ),
+          ),
+          Card(
+            elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextField(
+                    decoration: InputDecoration(labelText: "Title"),
+                    controller: titleController,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: "Amount"),
+                    controller: amountController,
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      print(
+                          "${titleController.text}  ${amountController.text}");
+                    },
+                    child: Text("Add transaction"),
+                    style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.purple),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
           Column(
@@ -64,7 +98,7 @@ class MyHomePage extends StatelessWidget {
                         width: 2,
                       )),
                       child: Text(
-                        tx.amount.toString(),
+                        "A: \$${tx.amount}", // tx.amount.toString(),
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
@@ -83,7 +117,7 @@ class MyHomePage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          tx.date.toString(),
+                          DateFormat("E, dd-MM-yyyy H:m").format(tx.date),
                           style: TextStyle(
                             color: Colors.grey,
                           ),
--- lib/transaction.dart
+++ lib/transaction.dart
@@ -1,15 +1,13 @@
-import 'package:flutter/cupertino.dart';
-
 class Transaction {
   final String id;
   final String title;
   final double amount;
-  final DateTime? date;
+  final DateTime date;
 
   Transaction({
-    @required this.id = "id",
-    @required this.title = "title",
-    @required this.amount = 99.99,
-    @required this.date,
+    required this.id,
+    required this.title,
+    required this.amount,
+    required this.date,
   });
 }
